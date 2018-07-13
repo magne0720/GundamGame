57be3c1fe5a1f0948da85d7527deb549a8adec6b
horutosou127 <chuunibyou127@gmail.com>
2018-07-13 10:13:26 +0900

    テスト用にemptyとscript作成

---
+++ Assets/Resources/Scripts/test2018_0713.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test2018_0713 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        Debug.Log("テストだよぉぉおお");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
